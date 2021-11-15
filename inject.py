086d3d0c4f69f4bd1b62f72be5510bbe3ca57f84
Yao-Ming Kuo <yaomingk93@gmail.com>
2021-11-15 11:39:18 +0100

    add cpu uart

--- inject.py
+++ inject.py
@@ -10,22 +10,30 @@ my_lib_path = os.path.abspath('./Classes')
 sys.path.append(my_lib_path)
 from telnet import Telnet
 from sem import SemIP
+from cpu import CPU
+
+# Configuration
+SEM_PORT = '/dev/ttyUSB2'
+SEM_BAUDRATE = 230400
+CPU_PORT = '/dev/ttyUSB1'
+CPU_BAUDRATE = 57600
 
 TESTS = 10000
-#BREAKPOINT = "0x00001ea8"
+ERROR_EACH = 70
+
 BREAKPOINT = "0x00001e14"
 PROGRAM_PATH = "/home/pi/gits/swervolf_scripts/elf/aes128.elf"
 PC_IDX = 29
 MCAUSE_IDX = 30
 MCAUSE_VALUE = "0x00000000"
 
-LOG_PATH = "rountine.log"
+LOG_PATH = "routine.log"
 ORIGINAL_PATH = "original.txt"
 INJECT_PATH = "inject.txt"
 INJECT_FILE = "./exu.txt"
-ERROR_EACH = 70
 
-# Time out, 10 secs
+
+# Time out, 5 secs
 TIMEOUT = 5      
 
 # How many success and errors
@@ -35,6 +43,7 @@ success = 0
 faults = 0
 hangs = 0
 mismatch = 0
+result_mismatch = 0
 
 # Set GPIO to program 
 GPIO.setmode(GPIO.BOARD)
@@ -44,17 +53,26 @@ log = open(LOG_PATH, "w")
 injFile = open(INJECT_FILE, "r")
 address = ""
 
-timeout = 0
-mcause = 0
+
 for num in range(TESTS+1):
+
+    timeout = 0
+    mcause = 0
+    result_error = 0
 
     # Reset board
     GPIO.output(40, False)
     time.sleep(0.5)
     GPIO.output(40, True)
-    sem = SemIP('/dev/ttyUSB2',230400)
+
+    # Connect SEM IP
+    sem = SemIP(SEM_PORT,SEM_BAUDRATE)
     sem.printOut()
     sem.injectState()
+
+    # Connect CPU UART
+    cpu = CPU(CPU_PORT,CPU_BAUDRATE)
+
     time.sleep(2.5)
     if num==0:
         f = open(ORIGINAL_PATH, "w")
@@ -78,51 +96,47 @@ for num in range(TESTS+1):
     if proc.poll()==None:
         # Connect and load the .elf file
         tn = Telnet("localhost", 4444)
-        tn.connect()
-        tn.run(PROGRAM_PATH)
 
-        # Run the program until "BREAKPOINT"
-        #if num==0:
-        #    tn.firstStep()
-        #else:
-        #    tn.step()
-
-        # Set timer
-        time1 = time.time()
-        timeout = 0
-
-        tn.firstStep()
-        tn.setBP(BREAKPOINT)
-        try: 
-            pc = tn.read_reg(PC_IDX).decode("utf-8")
-        except:
-            pc = BREAKPOINT
-            timeout = 1
-            print("ERROR: READ PC")
-            log.write("ERROR: READ PC\n")
-
+        status = 0
         try:
-            mcause = tn.read_reg(MCAUSE_IDX).decode("utf-8")
+            tn.connect()
+            status = 0
         except:
-            mcause = 1
             timeout = 1
-            print("ERROR: READ MCAUSE")
-            log.write("ERROR: READ MCAUSE\n")
+            print("ERROR: TELNET CONNECT")
+            status = 1
 
-    
-        while ((pc.find(BREAKPOINT) == -1) and (mcause.find(MCAUSE_VALUE) != -1)) :
-            tn.resume()
-            time.sleep(0.1)
-            tn.halt()
+        # If it is possible to connect, continue 
+        if status == 0:
             try:
+                tn.run(PROGRAM_PATH)
+            except:
+                timeout = 1
+                print("ERROR: TELNET LOADING PROGRAM")
+
+            # Set timer
+            time1 = time.time()
+
+            try:
+                tn.firstStep()
+            except:
+                timeout = 1
+                print("ERROR: STEPPING PROGRAM")
+
+            try:
+                tn.setBP(BREAKPOINT)
+            except:
+                timeout = 1
+                print("ERROR: SETTING BREAKPOINT")
+
+            try: 
                 pc = tn.read_reg(PC_IDX).decode("utf-8")
             except:
                 pc = BREAKPOINT
                 timeout = 1
                 print("ERROR: READ PC")
                 log.write("ERROR: READ PC\n")
-                break
-            
+
             try:
                 mcause = tn.read_reg(MCAUSE_IDX).decode("utf-8")
             except:
@@ -130,32 +144,64 @@ for num in range(TESTS+1):
                 timeout = 1
                 print("ERROR: READ MCAUSE")
                 log.write("ERROR: READ MCAUSE\n")
-                break
-            time2 = time.time()
-            if (time2-time1) > TIMEOUT:
-                timeout = 1
-                break
 
-        # Read all registers
-        aux = ""
-        if timeout==0:
-            for i in range(len(tn.regs)):
+            cpu_exit_status = "START"
+            while ((pc.find(BREAKPOINT) == -1) and (mcause.find(MCAUSE_VALUE) != -1)) :
+                tn.resume()
+                
+                if "START" in cpu_exit_status:
+                    cpu_exit_status = cpu.printOut()
+                time.sleep(0.1)
+
+                tn.halt()
+
+                if "FAIL" in cpu_exit_status:
+                    result_error = 1
+                    break
+
                 try:
-                    temporal_reg = tn.read_reg(i).decode("utf-8")
+                    pc = tn.read_reg(PC_IDX).decode("utf-8")
                 except:
-                    temporal_reg = ""
-                    print("ERROR: READ ALL REGS")
-                    log.write("ERROR: READ ALL REGS\n")
+                    pc = BREAKPOINT
+                    timeout = 1
+                    print("ERROR: READ PC")
+                    log.write("ERROR: READ PC\n")
+                    break
+                
+                try:
+                    mcause = tn.read_reg(MCAUSE_IDX).decode("utf-8")
+                except:
+                    mcause = 1
+                    timeout = 1
+                    print("ERROR: READ MCAUSE")
+                    log.write("ERROR: READ MCAUSE\n")
+                    break
+                time2 = time.time()
+                if (time2-time1) > TIMEOUT:
                     timeout = 1
                     break
 
-                aux = aux + temporal_reg.rstrip() + ","
+            # Read all registers
+            aux = ""
+            if timeout==0:
+                for i in range(len(tn.regs)):
+                    try:
+                        temporal_reg = tn.read_reg(i).decode("utf-8")
+                    except:
+                        temporal_reg = ""
+                        print("ERROR: READ ALL REGS")
+                        log.write("ERROR: READ ALL REGS\n")
+                        timeout = 1
+                        break
 
-        # Write the registers in file
-        f.write(aux + "\n")
-        f.flush()
+                    aux = aux + temporal_reg.rstrip() + ","
 
-        tn.exit()
+            # Write the registers in file
+            f.write(aux + "\n")
+            f.flush()
+
+            tn.exit()
+
         # Close OpenOCD
         time.sleep(0.5)
         proc.terminate()
